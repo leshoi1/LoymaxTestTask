329f373e847c655de22652b28d6f6d3e2dede733
leshoi1 <leshoiizlesa@gmail.com>
2019-07-16 04:59:37 +0600

    Models added Controllers added

---
+++ LoymaxTestTask/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoymaxTestTask.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string FamilyName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Funds { get; set; }
+    }
+}
